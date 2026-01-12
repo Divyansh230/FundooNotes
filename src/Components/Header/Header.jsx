146bdf8a6c5e96861bbd25b17fe6d8c27e46ee6d
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-01-12 12:39:24 +0530

    Some updation in the header section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,61 +1,95 @@
-import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
 import InputBase from "@mui/material/InputBase";
-import SearchIcon from "@mui/icons-material/Search";
-import MenuIcon from "@mui/icons-material/Menu";
-import RefreshIcon from "@mui/icons-material/Refresh";
-import SettingsIcon from "@mui/icons-material/Settings";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import Box from "@mui/material/Box";
-import "./Header.css";
+import AppsIcon from '@mui/icons-material/Apps';
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import ViewAgendaOutlinedIcon from "@mui/icons-material/ViewAgendaOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import AccountCircle from "@mui/icons-material/AccountCircle";
 
-const Header = () => {
+export default function KeepHeader() {
   return (
-    <AppBar position="static" elevation={1} className="bg-white">
-      <Toolbar className="d-flex align-items-center">
+    <AppBar
+      position="sticky"
+      sx={{
+        bgcolor: "#fff",
+        color: "#5f6368",
+        boxShadow: "none",
+        borderBottom: "1px solid #dadce0",
+      }}
+    >
+      <Toolbar sx={{ justifyContent: "space-between" }}>
 
-        {/* LEFT */}
-        <Box className="d-flex align-items-center gap-2">
+        {/* 🔵 LEFT */}
+        <Box sx={{ display: "flex", alignItems: "center", width: 240 }}>
           <IconButton>
             <MenuIcon />
           </IconButton>
 
-          <img
-            src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png"
-            alt="keep"
-            className="keep-logo"
-          />
-
-          <h5 className="m-0 text-secondary fw-normal">Keep</h5>
+          <Box sx={{ display: "flex", alignItems: "center", ml: 1 }}>
+            <img
+              src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png"
+              alt="keep"
+              width="40"
+              height="40"
+            />
+            <Typography sx={{ ml: 1, fontSize: 22 }}>
+              Keep
+            </Typography>
+          </Box>
         </Box>
 
-        {/* CENTER SEARCH */}
-        <Box className="search-container mx-auto d-flex align-items-center">
-          <SearchIcon className="text-secondary" />
-          <InputBase
-            placeholder="Search"
-            className="ms-2 w-100"
-          />
+        {/* 🟡 CENTER (SEARCH) */}
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: "flex",
+            justifyContent: "flex-start",
+            pl:6
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              bgcolor: "#f1f3f4",
+              px: 2,
+              height: 46,
+              borderRadius: 2,
+              width: "100%",
+              maxWidth: 720,
+            }}
+          >
+            <SearchIcon sx={{ mr: 1 }} />
+            <InputBase
+              placeholder="Search"
+              sx={{ width: "100%" }}
+            />
+          </Box>
         </Box>
 
-        {/* RIGHT */}
-        <Box className="d-flex align-items-center gap-3">
-          <IconButton>
-            <RefreshIcon />
-          </IconButton>
-          <IconButton>
-            <SettingsIcon />
-          </IconButton>
-          <IconButton>
-            <AccountCircleIcon />
-          </IconButton>
+        {/* 🟢 RIGHT */}
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            width: 240,
+          }}
+        >
+          <IconButton><RefreshIcon /></IconButton>
+          <IconButton><ViewAgendaOutlinedIcon /></IconButton>
+          <IconButton sx={{mr:5}}><SettingsOutlinedIcon /></IconButton>
+          <IconButton><AppsIcon/></IconButton>
+          <IconButton><AccountCircle /></IconButton>
         </Box>
 
       </Toolbar>
     </AppBar>
   );
-};
-
-export default Header;
+}
